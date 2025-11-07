03cc83be9309abeb9710918024cade4e3d37da0c
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-07 10:56:10 +0530

    question url change

--- client/src/Components/Test/Questions.jsx
+++ client/src/Components/Test/Questions.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export async function fetchQuestions() {
   try {
-    const res = await axios.get("http://localhost:5000/api/questions");
+    const res = await axios.get(`${API_BASE}/api/questions`);
     const formattedData = res.data.map((q) => ({
       id: q._id,                    
       question: q.question,
