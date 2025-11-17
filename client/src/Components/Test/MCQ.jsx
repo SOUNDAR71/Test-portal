db4d5c08f1b1e58bdc6b2f520421f20d31d13c44
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-17 16:15:06 +0530

    css Change

--- client/src/Components/Test/MCQ.jsx
+++ client/src/Components/Test/MCQ.jsx
@@ -6,7 +6,7 @@ const MCQ = () => {
     <div className="fixed top-0 left-0 w-full h-full  bg-gray-100">
       <div>
         <h1 className="text-center mt-20 font-bold text-2xl mt-5  ">Avaliable Tests</h1>
-        <div className="mt-10 bg-white fixed top-40 left-10 w-[350px] h-[170px] rounded-2xl bg-orange-400">
+        <div className="mt-10  fixed top-40 left-10 w-[350px] h-[170px] rounded-2xl bg-orange-300">
           <h1 className="text-center mt-7 font-bold text-lg ">Aptitude Test</h1>
           
           <Link to= "/test">
